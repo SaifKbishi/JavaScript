9fca1ab5820228d995a1aca43bef8dc5c8d94d44
Saif Kbishi <Saifkbishi@gmail.com>
2021-03-03 22:56:24 +0200

    movie

--- 34.3-movie_app.js
+++ 34.3-movie_app.js
@@ -1,3 +1,4 @@
+console.log('working movie');
 /**
  * Here is your key: cd09ef53
  * OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=cd09ef53
@@ -15,9 +16,40 @@
  * ratings object
  * on NEW search clear old searches
  */
- 
- const baseEndpoint = 'http://www.omdbapi.com/?apikey=cd09ef53&';
- 
- const searchMovie = async ()=>{
+ //http://www.omdbapi.com/?apikey=cd09ef53&t=braveheart
+ const cors = 'https://cors-anywhere.herokuapp.com/';
+ const baseEndpoint = 'https://www.omdbapi.com/?apikey=cd09ef53&';
+ const posterEndpoint = 'https://img.omdbapi.com/?apikey=cd09ef53&';//poster API
+ const moviesArray = [];
 
+
+ const searchMovie = async (query)=>{
+  try{
+   const requestMovies = await fetch(`${cors}${baseEndpoint}${query}`);
+   const moviesData = await requestMovies.json();
+   if (!requestMovies.ok) {  return handleError(moviesData.message); }
+   console.log('moviesData: ',moviesData);
+
+   let movieObj = {
+    title: moviesData.Title,
+    genre: moviesData.Genre,
+    year: moviesData.Year,
+    plot: moviesData.Plot,
+    director: moviesData.Director,
+    actors: moviesData.Actors,
+    ratings: moviesData.Ratings,
+   };
+   moviesArray.push(movieObj);
+   //console.log(movieObj);
+   displayMovie(movieObj); //display movie when all data is ready
+  }
+  catch(error){
+   console.log(`${error}, error fetch data`);
+  }
  }//searchMovie
+
+ searchMovie('t=braveheart');
+
+ const displayMovie = (aMovie)=>{
+  console.log(aMovie);
+ }//displayMovie
